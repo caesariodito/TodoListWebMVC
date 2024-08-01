2d9013644e4909af830a13e88d40326bbfd05d45
sesar <sesar@DESKTOP-B7GG7V7>
2024-08-01 11:11:38 +0700

    Add project files.

---
+++ TodoListWebMVC/Controllers/MiawController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TodoListWebMVC.Controllers
+{
+    public class MiawController : Controller
+    {
+
+        // Will error if uncommented because I don't have the view(?)
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+                 
+        // GET: /Miaw/
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+        
+
+        // GET: /Miaw/Anjay/ 
+        public string Anjay()
+        {
+            return "This is the Anjay action method...";
+        }
+    }
+}
---
+++ TodoListWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TodoListWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
