827cc4b70f636a50acc06d07e38f9b5757b719ee
caesariodito-windows-visualstudio2022 <caesariodito@gmail.com>
2024-08-01 12:57:32 +0700

    feat: bind controller and view

--- TodoListWebMVC/Controllers/MiawController.cs
+++ TodoListWebMVC/Controllers/MiawController.cs
@@ -13,9 +13,9 @@ namespace TodoListWebMVC.Controllers
         //}
                  
         // GET: /Miaw/
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
         
 
