3c364bca960707dfaacb876bea42a7cb39e859b1
caesariodito-windows-visualstudio2022 <caesariodito@gmail.com>
2024-08-02 10:32:23 +0700

    feat: add seeder for Todo data model

---
+++ TodoListWebMVC/Models/SeedData.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TodoListWebMVC.Data;
+using System;
+using System.Linq;
+
+namespace TodoListWebMVC.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new TodoListWebMVCContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<TodoListWebMVCContext>>()))
+            {
+                // Look for any movies.
+                if (context.Todo.Any())
+                {
+                    return;   // DB has been seeded
+                }
+                context.Todo.AddRange( // The seeder
+                    new Todo
+                    {
+                        Name = "Miaw",
+                        CreatedAt = DateTime.Parse("2021-08-01"),
+                        EditedAt = DateTime.Parse("2021-08-01"),
+                    },
+                    new Todo
+                    {
+                        Name = "MiawAWAW",
+                        CreatedAt = DateTime.Parse("2022-08-01"),
+                        EditedAt = DateTime.Parse("2022-08-01"),
+                    },
+                    new Todo
+                    {
+                        Name = "DAYM",
+                        CreatedAt = DateTime.Parse("2023-08-01"),
+                        EditedAt = DateTime.Parse("2023-08-01"),
+                    },
+                    new Todo
+                    {
+                        Name = "Holy sheeesh",
+                        CreatedAt = DateTime.Parse("2024-08-01"),
+                        EditedAt = DateTime.Parse("2024-08-01"),
+                    },
+                    new Todo
+                    {
+                        Name = "Rawrrrrr",
+                        CreatedAt = DateTime.Parse("2025-08-01"),
+                        EditedAt = DateTime.Parse("2025-08-02"),
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- TodoListWebMVC/Program.cs
+++ TodoListWebMVC/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using TodoListWebMVC.Data;
+using TodoListWebMVC.Models;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<TodoListWebMVCContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("TodoListWebMVCContext") ?? throw new InvalidOperationException("Connection string 'TodoListWebMVCContext' not found.")));
@@ -9,6 +10,12 @@ builder.Services.AddDbContext<TodoListWebMVCContext>(options =>
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
