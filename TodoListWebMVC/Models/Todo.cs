3488fe2855b3cb16d739a529d4f26848704dc440
caesariodito-windows-visualstudio2022 <caesariodito@gmail.com>
2024-08-05 14:37:14 +0700

    feat: add validation; finished the tutorial

--- TodoListWebMVC/Models/Todo.cs
+++ TodoListWebMVC/Models/Todo.cs
@@ -5,15 +5,27 @@ namespace TodoListWebMVC.Models
     public class Todo
     {
         public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string? Name { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string? Category { get; set; }
+
+        [StringLength(120)]
         public string? Description { get; set; }
+
         [Display(Name = "Created At")]
         [DataType(DataType.Date)]
         public DateTime? CreatedAt { get; set; }
+
         [Display(Name = "Edited At")]
         [DataType(DataType.Date)]
         public DateTime? EditedAt { get; set; }
+
         public bool IsFinished { get; set; }
     }
 }
